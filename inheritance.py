72a5d39f130cdd7e62b06f916a82dffcb540a124
Divinefavourchioma <divinefavour1811005@gmail.com>
2024-11-14 12:44:08 +0100

    this is my first commit

---
+++ inheritance.py
@@ -0,0 +1,72 @@
+# class Computer:
+#     def __init__(self):
+#        self.name = "Divine"
+#        self.age = 25
+    
+#     ###to update the age  ###
+#     def update(self):
+#         self.age = 55
+
+#         ### to compare two object
+#     def compare(self, other):
+#         if self.age == other.age:
+#             return True
+#         else:
+#             return False
+        
+# c1 = Computer()
+# c2 = Computer()
+
+
+# c1.name = "chioma"  # you can also change a variable attribyte
+# print(c1.name)
+# print(c1.age)
+        
+
+# c1.update()
+# print(c1.age)
+
+# print(c2.name)
+# print(c2.age)
+
+# if c1.compare(c2):
+#     print("same")
+# else:
+#     print("none")
+
+
+
+###class variable
+
+# class Car:
+#     wheels = 4
+
+#     def __init__(self):
+#         self.name = "Volvo"
+#         self.speed = "10km/h"
+        
+
+# c1 = Car()
+# c2 = Car()
+
+
+# c1.name = "Honda"
+# #car.wheels = 25
+
+# print(c1.name, c1.speed, c1.wheels)
+# print(c2.name, c2.speed, c2.wheels)
+
+
+
+# class Student:
+#     school_name = "Dangret"  ## static\ class variable
+
+#     def __init__(self, m1, m2, m3):
+#         self.m1 = m1
+#         self.m2 = m2
+
+#     def ave(self): ###this is an instance method
+#         return (self.m1 + self.m2 + self.m3) / 3
+
+
+
